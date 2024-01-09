3d5f00b24e82792e62be297dbd3029f9f273cadc
Nugar <cladeadenugar@gmail.com>
2024-01-10 00:12:44 +0100

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, lazy, Suspense } from "react";
+import React, { useState, lazy, Suspense, useEffect } from "react";
 import "./App.css";
 const Navbar = lazy(() => import("./components/Navbar"));
 const Header = lazy(() => import("./components/Header"));
@@ -7,6 +7,7 @@ const Article = lazy(() => import("./components/Article"));
 const Favorite = lazy(() => import("./components/Favorite"));
 const Question = lazy(() => import("./components/Question"));
 const Footer = lazy(() => import("./components/Footer"));
+import { HashLoader } from "react-spinners";
 
 const App = () => {
   const spaceGrotesk = {
@@ -20,74 +21,90 @@ const App = () => {
   };
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 4000);
+  }, []);
+
   return (
-    <div className="bg-black">
-      <Navbar
-        roboto={roboto}
-        spaceGrotesk={spaceGrotesk}
-        isMenuOpen={isMenuOpen}
-        setIsMenuOpen={setIsMenuOpen}
-      />
-      {!isMenuOpen && (
-        <div className="overflow-hidden">
-          <Suspense
-            fallback={
-              <div className="bg-black h-screen text-black">
-                Loading Header...
-              </div>
-            }
-          >
-            <Header roboto={roboto} clashDisplay={clashDisplay} />
-          </Suspense>
-          <Suspense
-            fallback={
-              <div className="bg-black h-screen text-black">
-                Loading Partners...
-              </div>
-            }
-          >
-            <Partners roboto={roboto} />
-          </Suspense>
-          <Suspense
-            fallback={
-              <div className="bg-black h-screen text-black">
-                Loading Article...
-              </div>
-            }
-          >
-            <Article roboto={roboto} clashDisplay={clashDisplay} />
-          </Suspense>
-          <Suspense
-            fallback={
-              <div className="bg-black h-screen text-black">
-                Loading Favorite...
-              </div>
-            }
-          >
-            <Favorite clashDisplay={clashDisplay} />
-          </Suspense>
-          <Suspense
-            fallback={
-              <div className="bg-black h-screen text-black">
-                Loading Question...
-              </div>
-            }
-          >
-            <Question roboto={roboto} clashDisplay={clashDisplay} />
-          </Suspense>
-          <Suspense
-            fallback={
-              <div className="bg-black h-screen text-black">
-                Loading Footer...
-              </div>
-            }
-          >
-            <Footer
-              roboto={roboto}
-              clashDisplay={clashDisplay}
-              spaceGrotesk={spaceGrotesk}
-            />
-          </Suspense>
+    <div className="bg-black h-screen">
+      {isLoaded ? (
+        <div className="bg-black">
+          <Navbar
+            roboto={roboto}
+            spaceGrotesk={spaceGrotesk}
+            isMenuOpen={isMenuOpen}
+            setIsMenuOpen={setIsMenuOpen}
+          />
+          {!isMenuOpen && (
+            <div className="overflow-hidden">
+              <Suspense
+                fallback={
+                  <div className="bg-black h-screen text-black">
+                    Loading Header...
+                  </div>
+                }
+              >
+                <Header roboto={roboto} clashDisplay={clashDisplay} />
+              </Suspense>
+              <Suspense
+                fallback={
+                  <div className="bg-black h-screen text-black">
+                    Loading Partners...
+                  </div>
+                }
+              >
+                <Partners roboto={roboto} />
+              </Suspense>
+              <Suspense
+                fallback={
+                  <div className="bg-black h-screen text-black">
+                    Loading Article...
+                  </div>
+                }
+              >
+                <Article roboto={roboto} clashDisplay={clashDisplay} />
+              </Suspense>
+              <Suspense
+                fallback={
+                  <div className="bg-black h-screen text-black">
+                    Loading Favorite...
+                  </div>
+                }
+              >
+                <Favorite clashDisplay={clashDisplay} />
+              </Suspense>
+              <Suspense
+                fallback={
+                  <div className="bg-black h-screen text-black">
+                    Loading Question...
+                  </div>
+                }
+              >
+                <Question roboto={roboto} clashDisplay={clashDisplay} />
+              </Suspense>
+              <Suspense
+                fallback={
+                  <div className="bg-black h-screen text-black">
+                    Loading Footer...
+                  </div>
+                }
+              >
+                <Footer
+                  roboto={roboto}
+                  clashDisplay={clashDisplay}
+                  spaceGrotesk={spaceGrotesk}
+                />
+              </Suspense>
+            </div>
+          )}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center w-full h-screen">
+          <HashLoader color="white" />
         </div>
       )}
     </div>
