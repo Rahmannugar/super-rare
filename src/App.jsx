a37c1426b271587fc7bcf8e7667cac68b54423f7
Nugar <cladeadenugar@gmail.com>
2024-01-10 00:03:57 +0100

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, lazy, Suspense } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
+const Navbar = lazy(() => import("./components/Navbar"));
 const Header = lazy(() => import("./components/Header"));
 const Partners = lazy(() => import("./components/Partners"));
 const Article = lazy(() => import("./components/Article"));
@@ -19,6 +19,7 @@ const App = () => {
     fontFamily: "'ClashDisplay', sans-serif",
   };
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="bg-black">
       <Navbar
@@ -30,13 +31,57 @@ const App = () => {
       {!isMenuOpen && (
         <div className="overflow-hidden">
           <Suspense
-            fallback={<div className="bg-black text-black">Loading...</div>}
+            fallback={
+              <div className="bg-black h-screen text-black">
+                Loading Header...
+              </div>
+            }
           >
             <Header roboto={roboto} clashDisplay={clashDisplay} />
+          </Suspense>
+          <Suspense
+            fallback={
+              <div className="bg-black h-screen text-black">
+                Loading Partners...
+              </div>
+            }
+          >
             <Partners roboto={roboto} />
+          </Suspense>
+          <Suspense
+            fallback={
+              <div className="bg-black h-screen text-black">
+                Loading Article...
+              </div>
+            }
+          >
             <Article roboto={roboto} clashDisplay={clashDisplay} />
+          </Suspense>
+          <Suspense
+            fallback={
+              <div className="bg-black h-screen text-black">
+                Loading Favorite...
+              </div>
+            }
+          >
             <Favorite clashDisplay={clashDisplay} />
+          </Suspense>
+          <Suspense
+            fallback={
+              <div className="bg-black h-screen text-black">
+                Loading Question...
+              </div>
+            }
+          >
             <Question roboto={roboto} clashDisplay={clashDisplay} />
+          </Suspense>
+          <Suspense
+            fallback={
+              <div className="bg-black h-screen text-black">
+                Loading Footer...
+              </div>
+            }
+          >
             <Footer
               roboto={roboto}
               clashDisplay={clashDisplay}
